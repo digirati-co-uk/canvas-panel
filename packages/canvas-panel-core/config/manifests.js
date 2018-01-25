3f9643a1e3585fa2f1b1361c782b1c89e6806030
stephenwf <stephen.fraser@digirati.com>
2018-01-25 21:11:55 +0000

    https manifest

--- packages/canvas-panel-core/config/manifests.js
+++ packages/canvas-panel-core/config/manifests.js
@@ -1,5 +1,5 @@
 module.exports = {
   main: 'https://iiif.library.nuigalway.ie/manifests/p135/memoir.json',
-  search: 'http://wellcomelibrary.org/iiif/b18035723/manifest',
+  search: 'https://wellcomelibrary.org/iiif/b18035723/manifest',
   p3manifest: require('../../../tests/patchwork'),
 };
