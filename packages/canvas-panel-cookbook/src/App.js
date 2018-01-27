f19347b75ba3a79bda64b2a61f827b0368754561
Stephen Fraser <stephen.fraser@digirati.com>
2018-01-27 14:26:01 +0000

    Changed router to use hash (netlify)

--- packages/canvas-panel-cookbook/src/App.js
+++ packages/canvas-panel-cookbook/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { HashRouter as Router, Route, NavLink } from 'react-router-dom';
 import Patchwork from './Patchwork';
 import FullPagePatchwork from './FullPagePatchwork';
 import './App.css';
