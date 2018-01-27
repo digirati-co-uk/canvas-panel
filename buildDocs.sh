f364c69556a780fb399bece98a616723398aa06b
Stephen Fraser <stephen.fraser@digirati.com>
2018-01-27 14:22:02 +0000

    Added exit codes

--- buildDocs.sh
+++ buildDocs.sh
@@ -6,12 +6,15 @@ rm -rf ./dist
 yarn run link
 
 # Build core (dependency)
-yarn run build-core
+yarn run build-core || { echo '@canvas-panel/core failed to build' ; exit 1; }
+
+# Link dependencies after building.
+yarn run link
 
 # Build cookbook example site (base)
-yarn run build-cookbook
+yarn run build-cookbook || { echo 'Cookbook failed to build' ; exit 1; }
 mv ./packages/canvas-panel-cookbook/dist .
 
 # Build documentation
-yarn run build-docs
+yarn run build-docs || { echo 'Styleguide failed to build' ; exit 1; }
 mv ./packages/canvas-panel-core/styleguide ./dist
