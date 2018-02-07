da3516ccafc2caa5c647e58bfdd9f6f13a3a2007
Stephen Fraser <stephenwf@users.noreply.github.com>
2018-02-07 11:57:45 +0000

    Site structure (#24)
    * updated ignore
    
    * Added markdown importing and new examples page structure

--- packages/canvas-panel-cookbook/nwb.config.js
+++ packages/canvas-panel-cookbook/nwb.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   type: 'react-app',
+  webpack: {
+    extra: {
+      module: {
+        rules: [
+          { test: /\.md$/, loader: 'html-loader!markdown-loader?gfm=true' },
+        ],
+      },
+    },
+  },
 };
