96709955b35acac6c6b528999a1ee992dee6cdaa
Stephen Fraser <stephenwf@users.noreply.github.com>
2018-02-11 22:06:57 +0000

    Fixed zoom in example, and improved UX (#34)

--- packages/canvas-panel-cookbook/src/PopOut.js
+++ packages/canvas-panel-cookbook/src/PopOut.js
@@ -30,41 +30,123 @@ const ViewportCover = props => {
   );
 };
 
-class PopOutViewport extends Component {
-  state = { minZoom: 1, position: null };
+// Height: 500px -> 80vh -> 80vh
+// Width: container -> full -> full
+// Pointer: none -> none -> all
 
-  setViewport = viewport => {
-    this.setState({ viewport: viewport });
+const DEFAULT_VIEW = 'DEFAULT_POSITION';
+const OPEN_VIEW = 'OPEN_VIEW';
+const INTERACTIVE_VIEW = 'INTERACTIVE_VIEW';
+
+class PopOutViewport extends Component {
+  static views = {
+    [DEFAULT_VIEW]: {
+      height: 500,
+      width: '100%',
+      transition: 'all .2s',
+      pointerEvents: 'none',
+    },
+    [OPEN_VIEW]: {
+      width: '100%',
+      height: '80vh',
+      transition: 'all .2s',
+      pointerEvents: 'initial',
+    },
+    [INTERACTIVE_VIEW]: {
+      width: '100%',
+      height: '80vh',
+      transition: 'all .2s',
+      pointerEvents: 'initial',
+    },
   };
 
-  setPosition = position => this.setState({ position });
+  static defaultProps = {
+    windowHeight: window.innerHeight,
+  };
+
+  state = {
+    currentView: DEFAULT_VIEW,
+    isAtTop: true,
+  };
+
+  lastScrollY = 0;
+
+  componentWillMount() {
+    this.setState({ isAtTop: window.scrollY > 50 });
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScrollThrottled);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScrollThrottled);
+  }
+
+  handleScrollThrottled = () => {
+    // Store the scroll value for later.
+    this.lastScrollY = window.scrollY;
+
+    // Prevent multiple rAF callbacks.
+    if (this.scheduledAnimationFrame) {
+      return;
+    }
+
+    this.scheduledAnimationFrame = true;
+    requestAnimationFrame(this.handleScroll);
+  };
+
+  handleScroll = () => {
+    this.scheduledAnimationFrame = false;
+    // Double tilde quicker than Math.floor, useful for scroll events.
+    const isAtTop = this.lastScrollY > 50; // 50 = threshold
+    if (isAtTop === false && this.state.currentView !== DEFAULT_VIEW) {
+      this.setState(() => ({
+        currentView: DEFAULT_VIEW,
+      }));
+    }
+  };
+
+  handleClick = () => {
+    if (this.state.currentView === DEFAULT_VIEW) {
+      return this.setState({ currentView: OPEN_VIEW });
+    }
+    if (this.state.currentView === OPEN_VIEW) {
+      return this.setState({ currentView: INTERACTIVE_VIEW });
+    }
+  };
+  onClose = () => this.setState({ currentView: DEFAULT_VIEW });
+  setViewport = viewport => this.setState({ viewport });
+  setPosition = position => {
+    if (position.isZoomedOut && this.state.currentView === INTERACTIVE_VIEW) {
+      this.setState({ currentView: DEFAULT_VIEW });
+    }
+  };
 
   render() {
-    const { position } = this.state;
+    const { currentView } = this.state;
     const { children, ...props } = this.props;
-
-    const style =
-      position && position.isZoomedOut
-        ? {
-            width: '100%',
-            height: 500,
-            transition: 'all .2s',
-          }
-        : {
-            width: '100%',
-            height: 700,
-            transition: 'all .2s',
-          };
-
     return (
-      <SizedViewport
-        style={style}
-        setRef={this.setViewport}
-        onUpdateViewport={this.setPosition}
-      >
-        <ViewportCover viewport={this.state.viewport} />
-        {functionOrMapChildren(children, props)}
-      </SizedViewport>
+      <div>
+        <button style={{ pointerEvents: 'initial' }} onClick={this.onClose}>
+          Close
+        </button>
+        <div onClick={this.handleClick}>
+          <div style={PopOutViewport.views[currentView]}>
+            <SizedViewport
+              style={{ width: '100%', height: '100%' }}
+              setRef={this.setViewport}
+              onUpdateViewport={this.setPosition}
+            >
+              <ViewportCover viewport={this.state.viewport} />
+              {functionOrMapChildren(children, {
+                ...props,
+                height: PopOutViewport.views[currentView].height,
+              })}
+            </SizedViewport>
+          </div>
+        </div>
+      </div>
     );
   }
 }
@@ -79,6 +161,7 @@ class PopOut extends Component {
             <PopOutViewport>
               <SingleTileSource viewportController={true}>
                 <OpenSeadragonViewport
+                  maxHeight={500}
                   useMaxDimensions={true}
                   osdOptions={{
                     visibilityRatio: 1,
