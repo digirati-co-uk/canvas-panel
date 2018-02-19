beec2d42c3097f97b4cd828d6626ced473692488
stephenwf <stephen.fraser@digirati.com>
2018-02-19 19:56:58 +0000

    Expanding galway examples

--- packages/galway-viewer/src/redux/sagas.js
+++ packages/galway-viewer/src/redux/sagas.js
@@ -1,6 +1,7 @@
 import { saga as manifestSaga } from './spaces/manifest';
 import { saga as structureSaga } from './spaces/structure';
+import { saga as annotationSaga } from './spaces/annotations';
 
-const sagas = [manifestSaga, structureSaga];
+const sagas = [manifestSaga, structureSaga, annotationSaga];
 
 export default sagas;
