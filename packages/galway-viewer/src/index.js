6be889bd051e5353924e5b386cf5d7694ae661ae
stephenwf <stephen.fraser@digirati.com>
2018-02-23 12:31:20 +0000

    Added netlify url

--- packages/galway-viewer/src/index.js
+++ packages/galway-viewer/src/index.js
@@ -4,7 +4,12 @@ import { render } from 'react-dom';
 import App from './App';
 
 render(
-  <App manifestUri="https://gist.githubusercontent.com/stephenwf/8c417a212866a21f48bd3ce9182e2f28/raw/cbfc217bd4c2f9311438a881db9a43fd015481cb/raw.json" />,
+  <App
+    manifestUri={
+      window.location.hash.substr(1) ||
+      'https://gist.githubusercontent.com/stephenwf/8c417a212866a21f48bd3ce9182e2f28/raw/cbfc217bd4c2f9311438a881db9a43fd015481cb/raw.json'
+    }
+  />,
   // <App manifestUri="https://edsilv.github.io/test-manifests/range-renderings.json" />,
   // <App manifestUri="https://wellcomelibrary.org/iiif/b18035723/manifest" />,
   document.querySelector('#app')
