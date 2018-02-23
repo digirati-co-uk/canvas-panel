9b7e9981c0bc33ef8f65463456694f87e76b3a65
stephenwf <stephen.fraser@digirati.com>
2018-02-23 12:33:12 +0000

    Remove old reference

--- packages/galway-viewer/src/index.js
+++ packages/galway-viewer/src/index.js
@@ -10,7 +10,6 @@ render(
       'https://gist.githubusercontent.com/stephenwf/8c417a212866a21f48bd3ce9182e2f28/raw/cbfc217bd4c2f9311438a881db9a43fd015481cb/raw.json'
     }
   />,
-  // <App manifestUri="https://edsilv.github.io/test-manifests/range-renderings.json" />,
   // <App manifestUri="https://wellcomelibrary.org/iiif/b18035723/manifest" />,
   document.querySelector('#app')
 );
