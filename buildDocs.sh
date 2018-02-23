25c61e2b505500d999954d31ab177f2851743851
stephenwf <stephen.fraser@digirati.com>
2018-02-23 12:31:20 +0000

    Added netlify url

--- buildDocs.sh
+++ buildDocs.sh
@@ -12,3 +12,8 @@ mv ./packages/canvas-panel-cookbook/dist .
 # Build documentation
 yarn run build-docs || { echo 'Styleguide failed to build' ; exit 1; }
 mv ./packages/canvas-panel-core/styleguide ./dist
+
+# Temporary: Build Galway viewer
+yarn run build-galway || { echo 'Galway failed to build' ; exit 1; }
+mv ./packages/galway-viewer/dist ./packages/galway-viewer/galway
+mv ./packages/galway-viewer/galway ./dist/galway
--- packages/galway-viewer/nwb.config.js
+++ packages/galway-viewer/nwb.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   type: 'react-app',
+  webpack: {
+    publicPath: '',
+  },
 };
