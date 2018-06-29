45ca13bd02b4591d14c02714d521b5cea4171e0c
Stephen Fraser <stephenwf@users.noreply.github.com>
2018-06-29 19:45:03 +0100

    CH-139 Mobile bug fixes (#169)
    * CH-139 Mobile bug fixes
    
    * CH-139 pass through alignment

--- packages/canvas-panel-full-page-plugin/src/components/ZoomButtons/ZoomButtons.js
+++ packages/canvas-panel-full-page-plugin/src/components/ZoomButtons/ZoomButtons.js
@@ -16,13 +16,47 @@ class ZoomButtons extends Component {
           className={bem.element('button').modifier('in')}
           onClick={onZoomIn}
         >
-          +
+          <svg
+            viewBox="0 0 400 400"
+            width="32"
+            height="32"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path fill="none" d="M-1-1h582v402H-1z" />
+            <g>
+              <ellipse ry="200" rx="200" cy="200.413" cx="200" fill="#fff" />
+              <ellipse rx="100" ry="100" cy="36" cx="943.5" fill="#fff" />
+              <path
+                d="M100 200h200M200 100v200"
+                fill="none"
+                stroke="#000"
+                strokeWidth="20"
+              />
+            </g>
+          </svg>
         </button>
         <button
           className={bem.element('button').modifier('out')}
           onClick={onZoomOut}
         >
-          –
+          <svg
+            viewBox="0 0 400 400"
+            width="32"
+            height="32"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path fill="none" d="M-1-1h582v402H-1z" />
+            <g>
+              <ellipse ry="200" rx="200" cy="200.413" cx="200" fill="#fff" />
+              <ellipse rx="100" ry="100" cy="36" cx="943.5" fill="#fff" />
+              <path
+                d="M100 200h200M200 300"
+                fill="none"
+                stroke="#000"
+                strokeWidth="20"
+              />
+            </g>
+          </svg>
         </button>
       </div>
     );
