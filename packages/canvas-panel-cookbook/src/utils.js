365c264d1ea51d9a0436b74ab354c8f69e61f554
Stephen Fraser <stephenwf@users.noreply.github.com>
2018-11-27 11:22:24 +0000

    SNYK-JS-FLATMAPSTREAM-72637 (#362)
    * Snyk 01
    
    * Removed event-stream from lock
    
    * Removed extra lock file

--- packages/canvas-panel-cookbook/src/utils.js
+++ packages/canvas-panel-cookbook/src/utils.js
@@ -1,9 +1,10 @@
-import lorem from 'lorem-ipsum-simple';
+const LOREM =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sollicitudin ante at massa ullamcorper placerat. Quisque in sollicitudin libero. Pellentesque id finibus eros. Nullam ultrices hendrerit diam nec efficitur. Fusce euismod felis leo, luctus malesuada massa luctus nec. Nulla faucibus posuere metus, vel semper dui semper quis. Etiam quis augue urna. Pellentesque ornare nisl nulla, eget euismod nisi euismod vel. Phasellus ultricies imperdiet cursus. Donec sollicitudin sagittis felis eu viverra.';
 
 export const lipsumN = paragraphs =>
-  Array.apply(null, Array(paragraphs)).map(() =>
-    lorem(100).replace(/\s+/g, ' ')
-  );
+  Array.apply(null, Array(paragraphs)).map(() => LOREM.replace(/\s+/g, ' '));
 
-export const lipsumP = paragraphs =>
-  lipsumN(paragraphs).map((lipsum, idx) => <p key={idx}>{lipsum}</p>);
+const lipsumP = paragraphs =>
+  Array.apply(null, Array(paragraphs))
+    .fill(LOREM)
+    .map((lipsum, idx) => <p key={idx}>{lipsum}</p>);
